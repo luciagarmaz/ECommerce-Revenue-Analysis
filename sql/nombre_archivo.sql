eebaf27857fd4b68b91ff67aea3a1ef6cfdb42d4
Lucia Vazquez Garmaz <vazquezgarmazl@gmail.com>
2026-03-31 15:51:28 +0200

    Create nombre_archivo.sql

---
+++ sql/nombre_archivo.sql
@@ -0,0 +1,33 @@
+sql/01_data_import_and_setup.sql
+#-- Data Import and Setup
+-- =====================================
+#-- This script enables local file loading and imports the dataset into MySQL
+
+-- Enable local file import
+SHOW VARIABLES LIKE 'local_infile';
+
+SET GLOBAL local_infile = 1;
+
+SHOW VARIABLES LIKE 'local_infile';
+
+#-- Set encoding to avoid character issues
+SET NAMES latin1;
+SET character_set_database = latin1;
+
+#-- Clearing table before re-import (optional)
+TRUNCATE TABLE online_retailraw;
+
+#-- Load data from CSV file
+LOAD DATA LOCAL INFILE 'C:/OnlineRetail.csv'
+INTO TABLE online_retailraw
+CHARACTER SET latin1
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES
+(InvoiceNo, StockCode, Description, Quantity, InvoiceDate, UnitPrice, CustomerID, Country);
+
+#-- Validate import
+SELECT COUNT(*) AS total_rows
+FROM online_retailraw;
+
